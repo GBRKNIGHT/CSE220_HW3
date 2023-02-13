25b4d02d7e798345b8475ea39b2a87b2c0a569a9
Yichen Li <yichen.li.1@stonybrook.edu>
2023-02-12 20:02:38 -0500

    Feb 12 20:02

--- Integers/integers.c
+++ Integers/integers.c
@@ -23,13 +23,12 @@ void repr_convert(char source_repr, char target_repr, unsigned int repr) {
                     print_hex(repr);
                     break;
                 case '2':
-                    // printf("Hello, World!");
                     if (is_smaller_than_zero(repr) == 0){
                         // if smaller than zero is false i.e. bigger than 0
                         print_hex(repr);
                     }else if (repr == 0){
                         print_hex(0);
-                    }else{printf("ffff\n");
+                    }else{
                         print_hex(repr + (unsigned int)1);
                     }
                     break;
@@ -39,7 +38,6 @@ void repr_convert(char source_repr, char target_repr, unsigned int repr) {
                     }else if (repr == 0){
                         print_hex(0);
                     }else{
-                        printf("ffff\n");
                         unsigned int temp;
                         temp = ~ repr;
                         temp = temp + 0x80000000;
@@ -47,6 +45,32 @@ void repr_convert(char source_repr, char target_repr, unsigned int repr) {
                     }
                     break;
                 case 'D':
+                    {
+                        unsigned int result = 0;
+                        if (repr == 0) {
+                            print_hex(repr);
+                        }else if (is_smaller_than_zero (repr) == 0){
+                            int temp_one_D_Positive = repr;
+                            for (int i = 0; i< 6 ; i++){
+                                unsigned int five_digits = temp_one_D_Positive % 10;
+                                five_digits = five_digits << (i*5);
+                                result = result | five_digits;
+                                temp_one_D_Positive = temp_one_D_Positive / 10;
+                            }
+                            print_hex(result);
+                        }else{
+                            int temp_one_D_Negative = repr;
+                            temp_one_D_Negative = ~ (temp_one_D_Negative);
+                            for (int i = 0; i < 6 ; i++){
+                                unsigned int five_digits = temp_one_D_Negative % 10;
+                                five_digits = five_digits << (i*5);
+                                result = result | five_digits;
+                                temp_one_D_Negative = temp_one_D_Negative / 10;
+                            }
+                            result = (result | 0xc0000000);
+                            print_hex(result);
+                        }
+                    }
                     break;
                 default:
                     break;
@@ -55,6 +79,68 @@ void repr_convert(char source_repr, char target_repr, unsigned int repr) {
 
         break;
         case '2':
+            {switch (target_repr){
+                case '1':{
+                    if(repr == 0x80000000){
+                        printf("undefined");
+                    }
+                    else if (repr == 0) {
+                        // if smaller than zero is false i.e. bigger than 0
+                        print_hex(0);
+                    }else if (is_smaller_than_zero(repr) == 0){
+                        print_hex(repr);
+                    }else{
+                        print_hex(repr - (unsigned int)1);
+                    }
+                    break;
+                    }
+                case '2':{
+                    print_hex(repr);
+                    break;
+                }
+                case 'S':{
+                    if (repr == 0){
+                        print_hex(0);
+                    }else if (is_smaller_than_zero(repr) == 0){
+                        print_hex(repr);
+                    }else{
+                        unsigned int temp;
+                        temp = (~ repr) - 1;
+                        temp = temp + 0x80000000;
+                        print_hex(temp);
+                    }
+                    break;
+                }
+                case 'D':{
+                        unsigned int result = 0;
+                        if (repr == 0) {
+                            print_hex(repr);
+                        }else if (is_smaller_than_zero (repr) == 0){
+                            int temp_two_D_Positive = repr;
+                            for (int i = 0; i< 6 ; i++){
+                                unsigned int five_digits = temp_two_D_Positive % 10;
+                                five_digits = five_digits << (i*5);
+                                result = result | five_digits;
+                                temp_two_D_Positive = temp_two_D_Positive / 10;
+                            }
+                            print_hex(result);
+                        }else{
+                            int temp_two_D_Negative = repr;
+                            temp_two_D_Negative = (~ (temp_two_D_Negative))-1;
+                            for (int i = 0; i < 6 ; i++){
+                                unsigned int five_digits = temp_two_D_Negative % 10;
+                                five_digits = five_digits << (i*5);
+                                result = result | five_digits;
+                                temp_two_D_Negative = temp_two_D_Negative / 10;
+                            }
+                            result = (result | 0xc0000000);
+                            print_hex(result);
+                        }
+                    }
+            }
+                
+                
+            }
             break;
         case 'S':
             break;
